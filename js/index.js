59d610297a30275ca1a8d34074b4e6f673970854
Jesua Escalona <jesuanextgmail.com>
2024-11-14 09:34:25 -0400

    Commit 1

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
-/**
- Se tiene de varios estudiantes su nombre, semestre y nota. También se sabe que la nota máxima es 20, y que los estudiantes aprueban con un mínimo de 10.
-Toda esta información de estudiantes se carga en un arreglo, y se necesita una función que retorne los estudiantes de un semestre dado.
-La estructura de un objeto estudiante es, p.ej.: {nombre: ‘Luis’, semestre: 5, nota: 14} 
-Función: estudiantesSemestre.
-Parámetros: estudiantes (array de objetos estudiante), semestre (un número).
-Retorno: array de objetos estudiante que pertenecen al semestre indicado. 
- */
 let estudiante = [
     { nombre: 'Luis', semestre: 5, nota: 14 },
     { nombre: 'Pedro', semestre: 6, nota: 12 },
